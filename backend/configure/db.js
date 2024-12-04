b0812abf40098f41e28e12abf9861caaf61ffafa
vivek-r-hiretanad <153304872+vivek-r-hiretanad@users.noreply.github.com>
2024-12-04 11:29:29 +0530

    Delete backend directory

--- backend/configure/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://vivek:Vivek123@cluster0.yphgl.mongodb.net/food-delivery').then(()=>console.log("Db connected!"))
-}
